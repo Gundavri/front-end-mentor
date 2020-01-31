3e1ac08511387c1476bbb988ecc393bd607a7ec6
Gundavri <m.mirianashvili99@gmail.com>
2020-01-31 17:30:27 +0400

    changed all id-s to classes

--- rock-paper-scissors-master/script.js
+++ rock-paper-scissors-master/script.js
@@ -4,13 +4,13 @@ var modalCloseBtn = document.getElementsByClassName('close')[0];
 var rulesBtn = document.getElementsByClassName('rules')[0];
 var textDivs = document.getElementsByClassName('selected-text');
 var lines = document.getElementsByClassName('line');
-var playerChoice = document.getElementById('player-choice');
-var enemyChoice = document.getElementById('enemy-choice');
-var playAgain = document.getElementById('play-again');
-var playAgainBtn = document.getElementById('play-again-btn');
-var playAgainText = document.getElementById('play-again-text');
-var playerUnderneath = document.getElementById('player-underneath');
-var enemyUnderneath = document.getElementById('enemy-underneath');
+var playerChoice = document.getElementsByClassName('player-choice');
+var enemyChoice = document.getElementsByClassName('enemy-choice');
+var playAgain = document.getElementsByClassName('play-again');
+var playAgainBtn = document.getElementsByClassName('play-again-btn');
+var playAgainText = document.getElementsByClassName('play-again-text');
+var playerUnderneath = document.getElementsByClassName('player-underneath');
+var enemyUnderneath = document.getElementsByClassName('enemy-underneath');
 var initialRow1 = document.getElementsByClassName('initial-row-1');
 var initialRow2 = document.getElementsByClassName('initial-row-2');
 var scoreDiv = document.getElementsByClassName('score-num')[0];
@@ -22,13 +22,23 @@ var signsWords = document.getElementsByClassName('signs-words');
 var gameWrappers = document.getElementsByClassName('game-wrapper');
 
 var signs = document.getElementsByClassName('wrapper');
+// Normal
 var paper = signs[0];
 var scissors = signs[1];
 var rock = signs[2];
 
+// Bonuses
+var spockBonus = signs[3];
+var scissorsBonus = signs[4];
+var paperBonus = signs[5];
+var lizardBonus = signs[6];
+var rockBonus = signs[7];
+
 
 var score = 0;
 var isTie = false;
+
+var normalMode = true;
 
 // CONSTANS
 
@@ -65,17 +75,46 @@ function rockCallback(event) {
     signClickedFunc('rock');
 }
 
+function paperBonusCallback(event) {
+    signClickedFunc('paperBonus');
+}
+
+function scissorsBonusCallback(event) {
+    signClickedFunc('scissorsBonus');
+}
+
+function rockBonusCallback(event) {
+    signClickedFunc('rockBonus');
+}
+
+function spockBonusCallback(event) {
+    signClickedFunc('spockBonus');
+}
+
+function lizardBonusCallback(event) {
+    signClickedFunc('lizardBonus');
+}
+
+
 function playAgainCallback(event) {
-    addEventListeners();
-    unchangeDisplayWhenChosen();
+    if(normalMode) {
+        addEventListeners();
+        unchangeDisplayWhenChosen();
+    }
+    else {
+        addEventListenersBonus();
+        unchangeDisplayWhenChosenBonus();
+    }
     unrandomizeAndDraw();
     undrawEnemy();
     undrawMiddle();
     unmoveUnderneathAndStyle();
     unmoveSelectedText();
+    toggleSwitch.disabled = false;
 }
 
 function modeSwitchCallback(event) {
+    normalMode = !normalMode;
     if(toggleSwitch.checked) {
         changeToBonusMode();
     } else {
@@ -105,6 +144,32 @@ function removeEventListeners() {
     rock.removeEventListener('click', rockCallback);
 }
 
+function addEventListenersBonus() {
+    paperBonus.addEventListener('click', paperBonusCallback);
+    scissorsBonus.addEventListener('click', scissorsBonusCallback);  
+    rockBonus.addEventListener('click', rockBonusCallback);
+    lizardBonus.addEventListener('click', lizardBonusCallback);
+    spockBonus.addEventListener('click', spockBonusCallback);
+    paperBonus.style.cursor = 'pointer';
+    scissorsBonus.style.cursor = 'pointer';
+    rockBonus.style.cursor = 'pointer';
+    lizardBonus.style.cursor = 'pointer';
+    spockBonus.style.cursor = 'pointer';
+}
+
+function removeEventListenersBonus() {
+    paperBonus.style.cursor = 'default';
+    scissorsBonus.style.cursor = 'default';
+    rockBonus.style.cursor = 'default';
+    lizardBonus.style.cursor = 'default';
+    spockBonus.style.cursor = 'default';
+    paperBonus.removeEventListener('click', paperBonusCallback);
+    scissorsBonus.removeEventListener('click', scissorsBonusCallback);
+    rockBonus.removeEventListener('click', rockBonusCallback);
+    lizardBonus.removeEventListener('click', lizardBonusCallback);
+    spockBonus.removeEventListener('click', spockBonusCallback);
+}
+
 function changeDisplaysWhenChosen(){
     textDivs[0].style.display = 'block';
     textDivs[1].style.display = 'block';
@@ -114,17 +179,34 @@ function changeDisplaysWhenChosen(){
     for(let ln of lines){
         ln.style.display = 'none';
     }
-    playerChoice.style.display = 'block';
-    enemyChoice.style.display = 'block';
-    playerUnderneath.style.display = 'block';
-    enemyUnderneath.style.display = 'block';
+    playerChoice[0].style.display = 'block';
+    enemyChoice[0].style.display = 'block';
+    playerUnderneath[0].style.display = 'block';
+    enemyUnderneath[0].style.display = 'block';
+}
+
+function changeDisplaysWhenChosenBonus(){
+    textDivs[2].style.display = 'block';
+    textDivs[3].style.display = 'block';
+    paperBonus.style.display = 'none';
+    scissorsBonus.style.display = 'none';
+    rockBonus.style.display = 'none';
+    spockBonus.style.display = 'none';
+    lizardBonus.style.display = 'none';
+    for(let ln of lines){
+        ln.style.display = 'none';
+    }
+    playerChoice[1].style.display = 'block';
+    enemyChoice[1].style.display = 'block';
+    playerUnderneath[1].style.display = 'block';
+    enemyUnderneath[1].style.display = 'block';
 }
 
 function unchangeDisplayWhenChosen(){
-    playerChoice.style.display = 'none';
-    enemyChoice.style.display = 'none';
-    playerUnderneath.style.display = 'none';
-    enemyUnderneath.style.display = 'none';
+    playerChoice[0].style.display = 'none';
+    enemyChoice[0].style.display = 'none';
+    playerUnderneath[0].style.display = 'none';
+    enemyUnderneath[0].style.display = 'none';
     textDivs[0].style.display = 'none';
     textDivs[1].style.display = 'none';
     paper.style.display = 'block';
@@ -135,29 +217,74 @@ function unchangeDisplayWhenChosen(){
     }
 }
 
+function unchangeDisplayWhenChosenBonus(){
+    playerChoice[1].style.display = 'none';
+    enemyChoice[1].style.display = 'none';
+    playerUnderneath[1].style.display = 'none';
+    enemyUnderneath[1].style.display = 'none';
+    textDivs[2].style.display = 'none';
+    textDivs[3].style.display = 'none';
+    paperBonus.style.display = 'block';
+    scissorsBonus.style.display = 'block';
+    rockBonus.style.display = 'block';
+    spockBonus.style.display = 'block';
+    lizardBonus.style.display = 'block';
+    for(let ln of lines){
+        ln.style.display = 'block';
+    }
+}
+
 
 function randomizeAndDraw(sign){
     result = {};
-    randNum = Math.floor(Math.random() * 3);
+    randNum = Math.floor(Math.random() * (normalMode ? 3 : 5));
     result.randNum = randNum;
 
     if(sign === 'paper') {
-        playerChoice.appendChild(paper.cloneNode(true));
+        playerChoice[0].appendChild(paper.cloneNode(true));
         if(randNum === 0) result.res = 0;
         else if(randNum === 1) result.res = -1;
-        else if(randNum === 2) result.res = 1;
+        else result.res = 1;
     } else if(sign === 'scissors'){
-        playerChoice.appendChild(scissors.cloneNode(true));
+        playerChoice[0].appendChild(scissors.cloneNode(true));
         if(randNum === 0) result.res = 1;
         else if(randNum === 1) result.res = 0;
-        else if(randNum === 2) result.res = -1;
+        else result.res = -1;
     } else if(sign === 'rock') {
-        playerChoice.appendChild(rock.cloneNode(true));
+        playerChoice[0].appendChild(rock.cloneNode(true));
         if(randNum === 0) result.res = -1;
         else if(randNum === 1) result.res = 1;
-        else if(randNum === 2) result.res = 0;
+        else result.res = 0;
+    } else if(sign === 'paperBonus') {
+        playerChoice[1].appendChild(paperBonus.cloneNode(true));
+        if(randNum === 2 || randNum === 4) result.res = 1;
+        else if(randNum === 3 || randNum === 0) result.res = -1;
+        else  result.res = 0;
+    } else if(sign === 'rockBonus') {
+        playerChoice[1].appendChild(rockBonus.cloneNode(true));
+        if(randNum === 0 || randNum === 3) result.res = 1;
+        else if(randNum === 1 || randNum === 4) result.res = -1;
+        else  result.res = 0;
+    } else if(sign === 'lizardBonus') {
+        playerChoice[1].appendChild(lizardBonus.cloneNode(true));
+        if(randNum === 1 || randNum === 4) result.res = 1;
+        else if(randNum === 3 || randNum === 0) result.res = -1;
+        else  result.res = 0;
+    } else if(sign === 'spockBonus') {
+        playerChoice[1].appendChild(spockBonus.cloneNode(true));
+        if(randNum === 0 || randNum === 2) result.res = 1;
+        else if(randNum === 3 || randNum === 1) result.res = -1;
+        else  result.res = 0;
+    } else if(sign === 'scissorsBonus') {
+        playerChoice[1].appendChild(scissorsBonus.cloneNode(true));
+        if(randNum === 1 || randNum === 3) result.res = 1;
+        else if(randNum === 2 || randNum === 4) result.res = -1;
+        else  result.res = 0;
     }
-    playerChoice.childNodes[0].style.display = 'block';
+
+    if(normalMode) playerChoice[0].childNodes[0].style.display = 'block';
+    else playerChoice[1].childNodes[0].style.display = 'block';
+
     if(!result.hasOwnProperty('res')) result.err = 'Something went wrong';
 
     return result;
@@ -166,36 +293,62 @@ function randomizeAndDraw(sign){
 
 function unrandomizeAndDraw(){
     result = {};
-    playerChoice.removeChild(playerChoice.childNodes[0]);
+    if(normalMode) playerChoice[0].removeChild(playerChoice[0].childNodes[0]);
+    else playerChoice[1].removeChild(playerChoice[1].childNodes[0]);
 }
 
 
 function drawEnemy(num){
-    if(num === 0) {
-        enemyChoice.appendChild(paper.cloneNode(true));
-    } else if(num === 1) {
-        enemyChoice.appendChild(scissors.cloneNode(true));
-    } else if(num === 2) {
-        enemyChoice.appendChild(rock.cloneNode(true));
+    if(normalMode) {
+        if(num === 0) {
+            enemyChoice[0].appendChild(paper.cloneNode(true));
+        } else if(num === 1) {
+            enemyChoice[0].appendChild(scissors.cloneNode(true));
+        } else {
+            enemyChoice[0].appendChild(rock.cloneNode(true));
+        }
+        enemyChoice[0].childNodes[0].style.display = 'block';
+        enemyChoice[0].childNodes[0].style.transform = 'translate(45%, 0)';
+    } else {
+        if(num === 0) {
+            enemyChoice[1].appendChild(scissorsBonus.cloneNode(true));
+        } else if(num === 1) {
+            enemyChoice[1].appendChild(paperBonus.cloneNode(true));
+        } else if(num === 2) {
+            enemyChoice[1].appendChild(rockBonus.cloneNode(true));
+        } else if(num === 3) {
+            enemyChoice[1].appendChild(lizardBonus.cloneNode(true));
+        } else {
+            enemyChoice[1].appendChild(rockBonus.cloneNode(true));
+        }
+        enemyChoice[1].childNodes[0].style.display = 'block';
+        enemyChoice[1].childNodes[0].style.transform = 'translate(45%, 0)';
     }
-    
-    enemyChoice.childNodes[0].style.display = 'block';
-    enemyChoice.childNodes[0].style.transform = 'translate(45%, 0)';
 }
 
 
 function undrawEnemy() {
-    enemyChoice.removeChild(enemyChoice.childNodes[0]);
+    if(normalMode) enemyChoice[0].removeChild(enemyChoice[0].childNodes[0]);
+    else enemyChoice[1].removeChild(enemyChoice[1].childNodes[0]);
 }
 
 
 function drawMiddle(txt) {
-    playAgain.style.display = 'block';
-    playAgainText.innerText = txt;
+    if(normalMode) {
+        playAgain[0].style.display = 'block';
+        playAgainText[0].innerText = txt;
+    } else {
+        playAgain[1].style.display = 'block';
+        playAgainText[1].innerText = txt;
+    }
 }
 
 function undrawMiddle() {
-    playAgain.style.display = 'none';
+    if(normalMode) {
+        playAgain[0].style.display = 'none';
+    } else {
+        playAgain[1].style.display = 'none';
+    }
 }
 
 
@@ -214,8 +367,11 @@ function drawRes(num) {
 
 
 function moveUnderneathAndStyle(res) {
-    playerUnderneath.style.display = 'none';
-    enemyUnderneath.style.display = 'none';
+    playerUnderneath[0].style.display = 'none';
+    enemyUnderneath[0].style.display = 'none';
+    playerUnderneath[1].style.display = 'none';
+    enemyUnderneath[1].style.display = 'none';
+
     if(res === 0) {
         isTie = true;
         return;
@@ -226,8 +382,10 @@ function moveUnderneathAndStyle(res) {
         tempUnderneaths.push(document.createElement('div'));
         tempUnderneaths[i].style.position = 'absolute';
         tempUnderneaths[i].style.borderRadius = '50%';
-        initialRow2[0].appendChild(tempUnderneaths[i]);
+        if(normalMode) initialRow2[0].appendChild(tempUnderneaths[i]);
+        else initialRow2[1].appendChild(tempUnderneaths[i]);
     }
+
 
     tempUnderneaths[0].style.backgroundColor = 'rgba(255, 255, 255, 0.01)';
     tempUnderneaths[0].style.zIndex = '-4';
@@ -257,10 +415,10 @@ function moveUnderneathAndStyle(res) {
 function unmoveUnderneathAndStyle(){
     if(!isTie){
         for(let i=0; i<3; i++){
-            initialRow2[0].removeChild(initialRow2[0].childNodes[initialRow2[0].childNodes.length-1]);
+            if(normalMode) initialRow2[0].removeChild(initialRow2[0].childNodes[initialRow2[0].childNodes.length-1]);
+            else initialRow2[1].removeChild(initialRow2[0].childNodes[initialRow2[0].childNodes.length-1]);
         }
     }
-    console.log(initialRow2[0].childNodes);
     isTie = false;
 }
 
@@ -268,11 +426,15 @@ function unmoveUnderneathAndStyle(){
 function moveSelectedText() {
     textDivs[0].style.width = '44%';
     textDivs[1].style.width = '44%';
+    textDivs[2].style.width = '44%';
+    textDivs[3].style.width = '44%';
 }
 
 function unmoveSelectedText() {
     textDivs[0].style.width = '30%';
     textDivs[1].style.width = '30%';
+    textDivs[2].style.width = '30%';
+    textDivs[3].style.width = '30%';
 }
 
 
@@ -281,9 +443,10 @@ function changeToNormalMode() {
     modalImgs[1].setAttribute('style', 'display:none !important');
     modalImgs[0].setAttribute('style', 'display:inline !important');
     // modalOpenFunc();
+    signsWords[1].style.display = 'none';
+    signsWords[0].style.display = 'block';
   
     
-    console.log('normal')
     gameWrappers[0].style.display = 'block';
     gameWrappers[1].style.display = 'none';
 }
@@ -293,20 +456,29 @@ function changeToBonusMode() {
     modalImgs[0].setAttribute('style', 'display:none !important');
     modalImgs[1].setAttribute('style', 'display:inline !important');
     // modalOpenFunc();
+    signsWords[0].style.display = 'none';
+    signsWords[1].style.display = 'block';
 
 
-    console.log(modalImgs);
     gameWrappers[0].style.display = 'none';
     gameWrappers[1].style.display = 'block';
+    addEventListenersBonus();
+
 }
 
 
 
 function signClickedFunc(sign){
-    removeEventListeners();
-    changeDisplaysWhenChosen();
+    toggleSwitch.disabled = true;
+    if(normalMode) {
+        removeEventListeners();
+        changeDisplaysWhenChosen();
+    }
+    else {
+        removeEventListenersBonus();
+        changeDisplaysWhenChosenBonus();
+    }
     result = randomizeAndDraw(sign);
-    console.log(result)
     setTimeout(() => {
         drawEnemy(result.randNum);
         setTimeout(() => {
@@ -319,7 +491,7 @@ function signClickedFunc(sign){
 
 // modalOpenFunc();
 addEventListeners();
-changeToBonusMode();
+// changeToBonusMode();
 // removeEventListeners();
 // changeDisplaysWhenChosen();
 // scissorsCallback();
@@ -338,8 +510,8 @@ modalCloseBtn.addEventListener('click', modalCloseFunc);
 
 rulesBtn.addEventListener('click', modalOpenFunc);
 
-playAgainBtn.addEventListener('click', playAgainCallback)
-
+playAgainBtn[0].addEventListener('click', playAgainCallback)
+playAgainBtn[1].addEventListener('click', playAgainCallback)
 
 toggleSwitch.addEventListener('change', modeSwitchCallback);
 
